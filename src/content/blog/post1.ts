265f1634c94450c5181d739d7d25e45b73b262b2
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-24 19:18:29 +0530

    Made it so blog cards can be made and displayed from ts files

---
+++ src/content/blog/post1.ts
@@ -0,0 +1,9 @@
+// src/content/blog/post1.ts
+
+export const post1 = {
+  title: "Setting Up Containers with Distrobox",
+  description: "Learn how to easily set up containers in a secure environment using Distrobox...",
+  date: "October 1, 2024",
+  image: "/images/distrobox.png",
+};
+
