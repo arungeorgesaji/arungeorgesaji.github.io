2ca3cd958b7f138bfeab75a476d8e72f01f43ad8
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-24 21:38:09 +0530

    Made the blog page much better

--- src/content/blog/post1.ts
+++ src/content/blog/post1.ts
@@ -1,9 +1,6 @@
-// src/content/blog/post1.ts
-
 export const post1 = {
   title: "Setting Up Containers with Distrobox",
   description: "Learn how to easily set up containers in a secure environment using Distrobox...",
   date: "October 1, 2024",
-  image: "/images/distrobox.png",
 };
 
