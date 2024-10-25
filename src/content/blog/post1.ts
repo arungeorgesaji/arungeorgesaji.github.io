048492693314f3607268b6b753762e0950b4aea8
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-25 23:47:28 +0530

    Made the blogs dynamic render blog pages when new ones are made

--- src/content/blog/post1.ts
+++
@@ -1,6 +0,0 @@
-export const post1 = {
-  title: "Setting Up Containers with Distrobox",
-  description: "Learn how to easily set up containers in a secure environment using Distrobox...",
-  date: "October 1, 2024",
-};
-
